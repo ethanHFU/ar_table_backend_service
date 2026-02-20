3b85455a160bb9ed144a8e0850b4188c968a4263
ethanHFU <ethanfuchs@outlook.de>
2026-02-20 20:36:45 +0100

    added support for arbitrary camera projector setups  via flip parameters

--- service/tasks/calibration.py
+++ service/tasks/calibration.py
@@ -2,6 +2,7 @@ from service.utils.file_utils import load_config
 from service.vision.camera import init_video_capture
 from service.utils.transform_utils import dist_to_map
 from service.vision.aruco import ArucoMarkerDetector
+import time
 import cv2 as cv
 import numpy as np
 import os
@@ -9,17 +10,28 @@ import os
 
 def _detect_markers_with_attempts(
         detector: ArucoMarkerDetector,
-        expected_count: int = None) -> tuple:
+        expected_count: int = None,
+        flip_H = None) -> tuple:
     """
     @public
     Try capturing markers up to MAX_NR_OF_ATTEMPTS times.
     Return detected corners and ids; exit if markers cannot be detected reliably.
     """
+    
+
     for attempt in range(1, MAX_NR_OF_ATTEMPTS + 1):
         frame = _get_most_recent_frame()
         if frame is None or np.mean(frame) < 5:
             print("Captured image is black, skipping frame.")
             continue
+        
+        if flip_H is not None:
+            if CFG["flip"]["horizontal"] or CFG["flip"]["vertical"]:
+                frame = cv.warpPerspective(
+                    frame,
+                    flip_H,
+                    (CFG["camera"]["width"], CFG["camera"]["height"])
+                )
 
         corners, ids = detector.detect(frame, DEBUG)
         if ids is not None:
@@ -104,15 +116,31 @@ def _calibrate_camera_to_projector():
     Compute the camera-to-projector homography.
     """
     aruco_grid = _setup_aruco_grid()    
-
-    # To respect mirrored projector setups for IT-Trans: flip grid -> detect markers -> unflip marker-coords 
-
     gt_grid_corners, gt_grid_ids = DETECTOR_PROJ.detect(aruco_grid)
 
+    
+    # front, rear, rear_upsidedown, front_upsidedown
+    # To respect mirrored projector setups for IT-Trans: flip grid -> detect markers -> unflip marker-coords 
+    # Build flip homography
+    flip_H = _build_flip_matrix(CFG["camera"]["width"], 
+                                CFG["camera"]["height"], 
+                                flip_h=CFG["flip"]["horizontal"], 
+                                flip_v=CFG["flip"]["vertical"])
+    
     cv.imshow(WNAME, aruco_grid)
     cv.waitKey(1)
+    proj_grid_corners, proj_grid_ids = _detect_markers_with_attempts(DETECTOR_PROJ, flip_H=flip_H)
+    # Suppose corners from aruco:
+    # corners: list of arrays of shape (1, 4, 2)
+    # flipped_corners = []
+    proj_grid_corners = list(proj_grid_corners)
+    for idx, marker in enumerate(proj_grid_corners):
+        pts_flipped = cv.perspectiveTransform(marker, flip_H)
+        proj_grid_corners[idx] = pts_flipped
+    proj_grid_corners = tuple(proj_grid_corners)
 
-    proj_grid_corners, proj_grid_ids = _detect_markers_with_attempts(DETECTOR_PROJ)
+    print("corners shape before", np.array(proj_grid_corners).shape)
+
     common_grid_corners, common_proj_corners, _ = _extract_common_corners(
         gt_grid_corners, gt_grid_ids, proj_grid_corners, proj_grid_ids
     )
@@ -166,7 +194,8 @@ def _calibrate_bounding_box(camera_to_projector_homography: np.ndarray) -> tuple
     Detect physical ArUco markers at table corners and compute bounding box homography.
     """
     cv.imshow(WNAME, WHITE_IMG)
-    cv.waitKey(1)
+    cv.waitKey(1)   
+    time.sleep(1) # Ensure white image is being displayed
 
     table_corners, table_ids = _detect_markers_with_attempts(DETECTOR_PHYS, expected_count=4)
 
@@ -179,6 +208,29 @@ def _calibrate_bounding_box(camera_to_projector_homography: np.ndarray) -> tuple
                            [0, CFG["projector"]["height"]]]], np.float32)
     bounding_box_homography, _ = cv.findHomography(src_pts, dst_pts)
     return bounding_box_homography
+
+
+def _build_flip_matrix(width, height, flip_h, flip_v):
+    M = np.eye(3, dtype=np.float32)
+
+    if flip_h:
+        Mh = np.array([
+            [-1, 0, width - 1],
+            [ 0, 1, 0],
+            [ 0, 0, 1]
+        ], dtype=np.float32)
+        M = Mh @ M
+
+    if flip_v:
+        Mv = np.array([
+            [1,  0, 0],
+            [0, -1, height - 1],
+            [0,  0, 1]
+        ], dtype=np.float32)
+        M = Mv @ M
+
+    return M
+
 
 def _get_most_recent_frame():
     # Cant rely on setting buffersize to 1, so we have to do this s***
@@ -195,6 +247,7 @@ def _get_most_recent_frame():
                     interpolation=cv.INTER_LINEAR
                 )
     return frame
+
 
 if __name__ == "__main__":
     DEBUG = True
@@ -226,6 +279,8 @@ if __name__ == "__main__":
                                         CFG["aruco_detection"]["detector_parameters"])
     
     WHITE_IMG = np.ones((CFG["projector"]["height"], CFG["projector"]["width"], 3), np.uint8) * 255
+    # text_test_img = r"C:\Users\ExploraVision\Src\ar_table_backend_service\text_img_test.png"
+    # TEST_IMG = cv.resize(cv.imread(text_test_img), (CFG["projector"]["width"], CFG["projector"]["height"]))
 
     cam_to_proj_H = _calibrate_camera_to_projector()
     bounding_box_H = _calibrate_bounding_box(cam_to_proj_H)
@@ -243,6 +298,7 @@ if __name__ == "__main__":
     if DEBUG:
         print("Displaying calibration bounding box in white.")
         debug_img = cv.warpPerspective(WHITE_IMG, bounding_box_H, (CFG["projector"]["width"], CFG["projector"]["height"]))
+        # debug_img = cv.warpPerspective(TEST_IMG, bounding_box_H, (CFG["projector"]["width"], CFG["projector"]["height"]))
         cv.imshow(WNAME, debug_img)
         cv.waitKey(0)
 
